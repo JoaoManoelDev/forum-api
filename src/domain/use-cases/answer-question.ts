724af0b2792dfe64b7ba0e88d2801e24fa432894
João Manoel <joaomanoeldev@gmail.com>
2025-07-03 18:35:13 -0300

    feat: add answer repository to AnswerQuestionUseCase

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,4 +1,5 @@
 import { Answer } from "../entities/answer"
+import { AnswersRepository } from "../repositories/answers-repository"
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
@@ -7,12 +8,16 @@ interface AnswerQuestionUseCaseRequest {
 }
 
 export class AnswerQuestionUseCase {
-  execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+  constructor(private answerRepository: AnswersRepository) {}
+
+  async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
     const answer = new Answer({
       content, 
       authorId: instructorId,
       questionId
     })
+
+    await this.answerRepository.create(answer)
 
     return answer
   }
