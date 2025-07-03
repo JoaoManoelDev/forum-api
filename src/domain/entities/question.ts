bdb99adb1d69a8df6285e52e0b2dfb4512d58927
João Manoel <joaomanoeldev@gmail.com>
2025-07-03 18:18:38 -0300

    feat: create basic question entity

---
+++ src/domain/entities/question.ts
@@ -0,0 +1,24 @@
+import { randomUUID } from "node:crypto"
+
+interface QuestionProps {
+  title: string
+  content: string
+  authorId: string
+}
+
+export class Question {
+  public id: string
+  public title: string
+  public content: string
+  public authorId: string
+
+  constructor(
+    props: QuestionProps,
+    id?: string
+  ) {
+    this.id = id ?? randomUUID()
+    this.title = props.title
+    this.content = props.content
+    this.authorId = props.authorId
+  }
+}
