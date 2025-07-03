f8527c8e26b9247b432be990f4e1fc7080edc366
João Manoel <joaomanoeldev@gmail.com>
2025-07-03 18:19:19 -0300

    feat: add answer question use case

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,19 @@
+import { Answer } from "../entities/answer"
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string
+  questionId: string
+  content: string
+}
+
+export class AnswerQuestionUseCase {
+  execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer({
+      content, 
+      authorId: instructorId,
+      questionId
+    })
+
+    return answer
+  }
+}
