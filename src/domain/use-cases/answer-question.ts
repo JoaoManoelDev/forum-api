22d72b3a4a51d974a51b880ce9b540ee012f26cb
João Manoel <joaomanoeldev@gmail.com>
2025-07-05 13:03:37 -0300

    refactor: use create method of Answer entity

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,5 +1,6 @@
-import { Answer } from "../entities/answer"
-import { AnswersRepository } from "../repositories/answers-repository"
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { Answer } from "@/domain/entities/answer"
+import { AnswersRepository } from "@/domain/repositories/answers-repository"
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
@@ -11,10 +12,10 @@ export class AnswerQuestionUseCase {
   constructor(private answerRepository: AnswersRepository) {}
 
   async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({
-      content, 
-      authorId: instructorId,
-      questionId
+    const answer = Answer.create({
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+      content
     })
 
     await this.answerRepository.create(answer)
