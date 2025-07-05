f918eac66988776515512f67000faefdc5b77d52
João Manoel <joaomanoeldev@gmail.com>
2025-07-05 11:49:19 -0300

    feat: create class Entity

---
+++ src/core/entity.ts
@@ -0,0 +1,15 @@
+import { UniqueEntityID } from "./unique-entity-id"
+
+export class Entity<Props> {
+  private _id: UniqueEntityID
+  protected props: Props
+
+  get id() {
+    return this._id
+  }
+
+  constructor(props: Props, id?: UniqueEntityID) {
+    this._id = id ?? new UniqueEntityID()
+    this.props = props
+  }
+}
