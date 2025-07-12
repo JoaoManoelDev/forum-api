d9615de1e285cece60bb5e9ff8e622781c03e8b1
João Manoel <joaomanoeldev@gmail.com>
2025-07-12 16:41:09 -0300

    feat: add geters in Question entity

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -2,6 +2,7 @@ import { UniqueEntityID } from "@/core/entities/unique-entity-id"
 import { Optional } from "@/core/types/optional"
 import { Entity } from "@/core/entities/entity"
 import { Slug } from "./value-objects/slug"
+import { differenceInDays } from "@/core/utils/date"
 
 interface QuestionProps {
   authorId: UniqueEntityID
@@ -14,6 +15,40 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get isNew() {
+    const result = differenceInDays(new Date(), new Date(this.props.createdAt))
+    
+    return result <= 3
+  }
+
   static create(
     props: Optional<QuestionProps, 'createdAt'>,
     id?: UniqueEntityID 
